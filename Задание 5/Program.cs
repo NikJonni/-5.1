861149f9ed5d8458ec0ae330b834de93e8ae7bf7
Никита <@LAPTOP-CDQK0D2E>
2021-11-23 16:44:44 +0300

    Добавьте файлы проекта.

---
+++ Задание 5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значения: ");
+            const int n = 7;
+            float sum = 0;
+            int[] array = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Введите '{0}' значение массива:  ", i);
+                int N = Convert.ToInt32(Console.ReadLine());
+                sum +=N;
+            }
+            float average = sum / n;
+            Console.WriteLine("Среднее значение массива: {0:f1}",average);
+            Console.ReadKey();
+        }
+    }
+}
